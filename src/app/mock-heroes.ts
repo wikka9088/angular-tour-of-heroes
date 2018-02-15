1d63cb043eedb6d9fb9fac83a86c67036d7e9b9c
V+ <wikka9088@gmail.com>
2018-02-14 22:58:48 -0500

    create mock heroes and display

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import  { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Java' },
+    { id: 12, name: 'PHP' },
+    { id: 13, name: 'JavaScript' },
+    { id: 14, name: 'TypeScript' },
+    { id: 15, name: 'Git' },
+    { id: 16, name: 'SQL' },
+];
