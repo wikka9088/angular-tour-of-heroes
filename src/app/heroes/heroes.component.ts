36ced0bbd0a9d309a39d1383fc6f13f812f5bcaf
V+ <wikka9088@gmail.com>
2018-02-19 23:38:52 -0500

    fix the bug, and the heroes display fine.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-import { Hero} from '../hero';
-
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectHero: Hero;
 
-  onSelect(hero: Hero): void {
-      this.selectHero = hero;
-  }
+    heroes = HEROES;
 
-  heroes: HEROES;
+    selectedHero: Hero;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() {
+    }
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
