c54a4fbfb57073b0e6bc6f61bbba8fa18bfbca7b
V+ <wikka9088@gmail.com>
2018-02-22 23:43:47 -0500

    add comments

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,11 +14,12 @@ export class HeroesComponent implements OnInit {
 
     selectedHero: Hero;
 
-
+    //When Angular creates a HeroesComponent, the Dependency Injection system sets the heroService parameter to the singleton instance of HeroService.
     constructor(private heroService: HeroService) {
 
     }
 
+    //call getHeroes() inside the ngOnInit lifecycle hook and let Angular call ngOnInit at an appropriate time after constructing a HeroesComponent instance.
     ngOnInit() {
         this.getHeroes();
     }
@@ -28,6 +29,10 @@ export class HeroesComponent implements OnInit {
     }
 
     getHeroes(): void {
+        // If this function just fetched synchronously: this.heroes = this.heroService.getHeroes(); The HeroService must wait for the server to respond, getHeroes() cannot return immediately with hero data, and the browser will not block while the service waits.
+        // So HeroService.getHeroes() must have an asynchronous signature of some kind.
+        // The new version waits for the Observable to emit the array of heroes— which could happen now or several minutes from now. Then subscribe passes the emitted array to the callback, which sets the component's heroes property.
+        // This asynchronous approach will work when the HeroService requests heroes from the server.
         this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
     }
 }
