c780f68f0bb9497b77496038b5d8e0e94350cf7e
V+ <wikka9088@gmail.com>
2018-02-25 19:32:49 -0500

    set up route matching the URL to path: heroes, and displaying the HeroesComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {HeroesComponent} from "./heroes/heroes.component";
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+    //Add an @NgModule.exports array with RouterModule in it.
+    //Exporting RouterModule makes router directives available for use in the AppModule components that will need them.
+    exports: [RouterModule]
+
+  //generally don't declare components in a routing module so you can delete the @NgModule.declarations array and delete CommonModule references too.
+  // imports: [
+  //   CommonModule
+  // ],
+  // declarations: [],
 })
+
+const routes: Routes = [
+    {path: 'heroes', component: HeroesComponent}
+];
 export class AppRoutingModule { }
