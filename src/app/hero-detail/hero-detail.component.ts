9e1fd78bfd779f0a0d7c74477ec3330671d377ed
V+ <wikka9088@gmail.com>
2018-02-25 22:16:45 -0500

    add go back button

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
+import {ActivatedRoute} from "@angular/router";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,9 +12,26 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+      private route: ActivatedRoute, //The ActivatedRoute holds information about the route to this instance of the HeroDetailComponent. This component is interested in the route's bag of parameters extracted from the URL. The "id" parameter is the id of the hero to display.
+      private heroService: HeroService,
+      private location: Location, //The location is an Angular service for interacting with the browser. You'll use it later to navigate back to the view that navigated here.
+  ) { }
 
   ngOnInit() {
+      this.getHero();
+  }
+
+  getHero(): void {
+      //The route.snapshot is a static image of the route information shortly after the component was created.
+      //The paramMap is a dictionary of route parameter values extracted from the URL. The "id" key returns the id of the hero to fetch.
+      //Route parameters are always strings. The JavaScript (+) operator converts the string to a number, which is what a hero id should be.
+      const id= +this.route.snapshot.paramMap.get('id');
+      this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+      this.location.back();
   }
 
 }
