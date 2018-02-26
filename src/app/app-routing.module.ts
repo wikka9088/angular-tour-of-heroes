b3efe38b03e183a327ed481fb871f3484696ead2
V+ <wikka9088@gmail.com>
2018-02-25 20:25:12 -0500

    first must initialize the router and start it listening for browser location changes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,12 @@ import {HeroesComponent} from "./heroes/heroes.component";
 @NgModule({
     //Add an @NgModule.exports array with RouterModule in it.
     //Exporting RouterModule makes router directives available for use in the AppModule components that will need them.
-    exports: [RouterModule]
+    exports: [RouterModule],
+
+
+    //The forRoot() method supplies the service providers and directives needed for routing,
+    // and performs the initial navigation based on the current browser URL.
+    imports: [RouterModule.forRoot(routes)],
 
   //generally don't declare components in a routing module so you can delete the @NgModule.declarations array and delete CommonModule references too.
   // imports: [
