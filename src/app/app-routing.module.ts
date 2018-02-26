947d2d3e16ccf585690154f9e39874864bae4885
V+ <wikka9088@gmail.com>
2018-02-25 21:29:46 -0500

    remove herodetail component from heroes Component, and add links instead

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {HeroesComponent} from "./heroes/heroes.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 
 @NgModule({
     //Add an @NgModule.exports array with RouterModule in it.
@@ -25,5 +26,6 @@ const routes: Routes = [
     {path: 'heroes', component: HeroesComponent},
     {path: 'dashboard', component: DashboardComponent},
     {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    {path: 'detail/:id', component: HeroDetailComponent},
 ];
 export class AppRoutingModule { }
