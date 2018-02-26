64b21d8b2a964b7defc47cc4422c8ed81bf67d2b
V+ <wikka9088@gmail.com>
2018-02-25 20:59:16 -0500

    add route of heroes and dashboard to app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {HeroesComponent} from "./heroes/heroes.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 @NgModule({
     //Add an @NgModule.exports array with RouterModule in it.
@@ -21,6 +22,8 @@ import {HeroesComponent} from "./heroes/heroes.component";
 })
 
 const routes: Routes = [
-    {path: 'heroes', component: HeroesComponent}
+    {path: 'heroes', component: HeroesComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
 ];
 export class AppRoutingModule { }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from "../hero";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1,5))
+  }
 
   ngOnInit() {
   }
