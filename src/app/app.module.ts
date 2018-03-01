6176f36bf0572d922ef2929469ead416b0a2f18d
V+ <wikka9088@gmail.com>
2018-02-28 23:16:47 -0500

    install angular-in-memory-web-api to mock a DB, and update hero service to fetch heroes and send message through http to mock remote server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import {HttpClientModule} from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 
 @NgModule({
@@ -24,7 +27,16 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+        InMemoryDataService, { dataEncapsulation: false }
+    )
+
   ],
   providers: [
       HeroService,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,38 @@
-import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
+import {Observable} from 'rxjs/Observable';
 import {of} from "rxjs/observable/of";
-import { MessageService } from './message.service';
+import {MessageService} from './message.service';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators'; //
 
 @Injectable()
 export class HeroService {
+    private heroesUrl = 'api/heroes'; // URL to web api
+    constructor(private http: HttpClient, private messageService: MessageService) {
 
-  constructor(private messageService : MessageService) {
+    }
 
-  }
+    getHero(id: number): Observable<Hero> {
+        this.messageService.add(`HeroService: fetched hero id=${id}`);
+        return of(HEROES.find(hero => hero.id === id));
+    }
 
-  getHero(id: number): Observable<Hero>
-  {
-      this.messageService.add(`HeroService: fetched hero id=${id}`);
-      return of(HEROES.find(hero => hero.id === id));
-  }
+    /** GET heroes from the server */
+    getHeroes(): Observable<Hero[]> {
+        return this.http.get<Hero[]>(this.heroesUrl)
+            .pipe(
+                catchError(this.handleError('getHeroes', []))
+            );
+        //pipe like try-catch, throws Exceptions
+    }
 
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
-  }
+    /**
+     * Log a HeroService message with the MessageService
+     */
+    private log(message: string) {
+        this.messageService.add('HeroService: ' + message);
+    }
 
 }
