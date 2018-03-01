2712176238f510314d8f88bf73efcf96ea115945
V+ <wikka9088@gmail.com>
2018-02-28 23:21:54 -0500

    update get hero by id and error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,17 +15,23 @@ export class HeroService {
     }
 
     getHero(id: number): Observable<Hero> {
-        this.messageService.add(`HeroService: fetched hero id=${id}`);
-        return of(HEROES.find(hero => hero.id === id));
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.get<Hero>(url)
+            .pipe(
+                tap(_ => this.log(`fetched hero id=${id}`)),
+                catchError(this.handleError<Hero>(`getHero id=${id}`))
+            )
     }
 
     /** GET heroes from the server */
-    getHeroes(): Observable<Hero[]> {
+    /** GET heroes from the server */
+    getHeroes (): Observable<Hero[]> {
         return this.http.get<Hero[]>(this.heroesUrl)
             .pipe(
+                tap(heroes => this.log(`fetched heroes`)),
                 catchError(this.handleError('getHeroes', []))
             );
-        //pipe like try-catch, throws Exceptions
+        //pipe is like try-catch to throw Exception
     }
 
     /**
