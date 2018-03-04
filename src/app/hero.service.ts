4e5935be9233c45edad5cad26e424e3e51ad9b28
V+ <wikka9088@gmail.com>
2018-03-04 17:08:08 -0500

    add search heroes component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -74,4 +74,16 @@ export class HeroService {
             )
     }
 
+    /* GET heroes whose name contains search term */
+    searchHero(term: string): Observable<Hero[]> {
+        if (!term.trim()) {
+            return of([]);
+        }
+        return this.http.get<Hero[]>(`api/heroes/?name=${term}`)
+            .pipe(
+                tap(_ => this.log(`found heroes matching "${term}"`)),
+                catchError(this.handleError<Hero[]>('searchHeroes', []))
+            )
+    }
+
 }
